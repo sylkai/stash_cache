589b57f40e9b70fedf35a2a54219486933c1a641
sylkai <sylkai@users.noreply.github.com>
2014-07-31 15:02:27 -0700

    initial fragment work

---
+++ app/src/main/java/com/geekeclectic/android/stashcache/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.geekeclectic.android.stashcache;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
+
+/**
+ * Created by sylk on 7/31/2014.
+ */
+public abstract class SingleFragmentActivity extends Activity {
+
+    protected abstract Fragment createFragment();
+
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/geekeclectic/android/stashcache/StashOverviewFragment.java
@@ -0,0 +1,16 @@
+package com.geekeclectic.android.stashcache;
+
+import android.app.Activity;
+import android.app.ListFragment;
+
+/**
+ * Created by sylk on 7/31/2014.
+ */
+public class StashOverviewFragment extends ListFragment {
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+}
