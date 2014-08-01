0b0ff68410174fc7b62745884b8ec10f64b8b628
sylkai <sylkai@users.noreply.github.com>
2014-08-01 14:30:37 -0700

    creating pattern class and fragment layout.

---
+++ app/src/main/java/com/geekeclectic/android/stashcache/Pattern.java
@@ -0,0 +1,64 @@
+package com.geekeclectic.android.stashcache;
+
+import java.util.UUID;
+
+public class Pattern {
+
+    private UUID mId;
+    private String mTitle;
+    private String mDesigner;
+    private boolean mIsKit;
+    private int mWidth;
+    private int mHeight;
+
+    public Pattern() {
+        // Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+
+    @Override
+    public String toString() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public boolean isKit() {
+        return mIsKit;
+    }
+
+    public void setKit(boolean kit) {
+        mIsKit = kit;
+    }
+
+    public void setDesigner(String designer) {
+        mDesigner = designer;
+    }
+
+    public String getDesigner() {
+        return mDesigner;
+    }
+
+    public void setWidth(int width) {
+        mWidth = width;
+    }
+
+    public int getWidth() {
+        return mWidth;
+    }
+
+    public void setHeight(int height) {
+        mHeight = height;
+    }
+
+    public int getHeight() {
+        return mHeight;
+    }
+
+}
