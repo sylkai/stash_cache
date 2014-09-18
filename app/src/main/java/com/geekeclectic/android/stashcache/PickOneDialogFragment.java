bbd24d985f5cabd9f98f32c583acfca694efda56
sylkai <sylkai@users.noreply.github.com>
2014-09-17 17:48:10 -0700

    Continued work on dialogs, taking a moment to rebase.

--- app/src/main/java/com/geekeclectic/android/stashcache/PickOneDialogFragment.java
+++ app/src/main/java/com/geekeclectic/android/stashcache/PickOneDialogFragment.java
@@ -10,7 +10,7 @@ import android.support.v4.app.DialogFragment;
 /**
  * Created by sylk on 9/15/2014.
  */
-public class PickOneFragment extends DialogFragment {
+public class PickOneDialogFragment extends DialogFragment {
 
     static final String TAG = "PickOneDialog";
 
