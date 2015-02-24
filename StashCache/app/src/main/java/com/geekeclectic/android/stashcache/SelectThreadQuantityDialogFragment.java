f03602d370cbe5cf7d387195d185e50805bc3092
sylkai <sylkai@users.noreply.github.com>
2015-02-24 00:07:04 -0800

    Addition to the stash test file, minor clean-up.

--- StashCache/app/src/main/java/com/geekeclectic/android/stashcache/SelectThreadQuantityDialogFragment.java
+++ StashCache/app/src/main/java/com/geekeclectic/android/stashcache/SelectThreadQuantityDialogFragment.java
@@ -10,12 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.UUID;
 
 /**
