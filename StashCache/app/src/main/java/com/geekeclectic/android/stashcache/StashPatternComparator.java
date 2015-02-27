6c0ff04e2eaaf373df308c928c467e3e146cd714
sylkai <sylkai@users.noreply.github.com>
2015-02-26 16:45:34 -0800

    Null pointer exception issues cleaned up (necessary to deal with them because of the sorting called every time the fragment is created/loaded).

--- StashCache/app/src/main/java/com/geekeclectic/android/stashcache/StashPatternComparator.java
+++ StashCache/app/src/main/java/com/geekeclectic/android/stashcache/StashPatternComparator.java
@@ -9,11 +9,25 @@ public class StashPatternComparator implements Comparator<StashPattern> {
 
     @Override
     public int compare(StashPattern pattern1, StashPattern pattern2) {
-        if (pattern1.getSource().equals(pattern2.getSource())) {
-            return pattern1.getPatternName().compareTo(pattern2.getPatternName());
+        if (pattern1.getSource() != null && pattern2.getSource() != null && pattern1.getSource().equals(pattern2.getSource())) {
+            if (pattern1.getPatternName() != null && pattern2.getPatternName() != null) {
+                return pattern1.getPatternName().compareTo(pattern2.getPatternName());
+            } else if (pattern1.getPatternName() == null && pattern2.getPatternName() == null) {
+                return 0;
+            } else if (pattern1.getPatternName() == null) {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else if (pattern1.getSource() != null && pattern2.getSource() != null) {
+            return pattern1.getSource().compareTo(pattern2.getSource());
+        } else if (pattern1.getSource() == null && pattern2.getSource() == null) {
+            return 0;
+        } else if (pattern1.getSource() == null) {
+            return 1;
+        } else {
+            return -1;
         }
-
-        return pattern1.getSource().compareTo(pattern2.getSource());
     }
 
 }
