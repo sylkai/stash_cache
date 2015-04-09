94a8dcbf80ad33c61d4faa43d11a88eb9778c63a
sylkai <sylkai@users.noreply.github.com>
2015-04-09 14:53:07 -0700

    Reorganization of StashData functions into logical groupings that should be easier to follow.

--- StashCache/app/src/main/java/com/geekeclectic/android/stashcache/StashData.java
+++ StashCache/app/src/main/java/com/geekeclectic/android/stashcache/StashData.java
@@ -41,6 +41,7 @@ public class StashData {
     private ArrayList<UUID> mStashEmbellishmentList;
     private ArrayList<UUID> mShoppingEmbellishmentList;
 
+    // general stash functions
     private StashData(Context appContext) {
         // set variables
         mAppContext = appContext;
@@ -90,167 +91,42 @@ public class StashData {
         return sStash;
     }
 
-    public void setThreadData(HashMap<UUID, StashThread> threadMap, ArrayList<UUID> threadList, ArrayList<UUID> stashList) {
-        // set the loaded threadmap from JSON
-        mThreadsData = threadMap;
-        mThreadsList = threadList;
-        mStashThreadsList = stashList;
-    }
-
-    public void setThreadShoppingList(ArrayList<UUID> shoppingList) {
-        // store the shopping list provided by the shoppinglist creator
-        mShoppingThreadsList = shoppingList;
-    }
-
-    public HashMap<UUID, StashThread> getThreadData() {
-        // pass threadmap for saving stash/building links
-        return mThreadsData;
-    }
-
-    public ArrayList<UUID> getThreadList() {
-        // pass master threadlist for list adapters
-        return mThreadsList;
-    }
-
-    public ArrayList<UUID> getThreadStashList() {
-        // pass the stash-only threadlist for list adapters
-        return mStashThreadsList;
-    }
-
-    public ArrayList<UUID> getThreadShoppingList() {
-        // pass the shopping threadlist for list adapters
-        Collections.sort(mShoppingThreadsList, new StashThreadComparator(mAppContext));
-        return mShoppingThreadsList;
-    }
-
-    public void addThreadToStash(UUID threadId) {
-        // needed to update the list as the user edits the quantities of thread owned
-        // only one entry per thread on the list
-        if (!mStashThreadsList.contains(threadId)) {
-            mStashThreadsList.add(threadId);
+    public boolean saveStash() {
+        // called at appropriate lifecycle events to save the stash to file
+        try {
+            mSerializer.saveStash(sStash);
+            Log.d(TAG, "stash saved to file");
+            return true;
+        } catch (Exception e) {
+            Log.e(TAG, "Error saving stash: ", e);
+            return false;
         }
     }
 
-    public void removeThreadFromStash(UUID threadId) {
-        // needed to update the list as the user edits the quantities of thread owned
-        mStashThreadsList.remove(threadId);
+    public void deleteStash() {
+        // clears ALL stash data and then saves it
+        mEmbellishmentData.clear();
+        mEmbellishmentList.clear();
+        mStashEmbellishmentList.clear();
+        mShoppingEmbellishmentList.clear();
+
+        mFabricData.clear();
+        mFabricList.clear();
+        mStashFabricList.clear();
+
+        mThreadsData.clear();
+        mThreadsList.clear();
+        mStashThreadsList.clear();
+        mShoppingThreadsList.clear();
+
+        mPatternsData.clear();
+        mFabricForList.clear();
+
+        saveStash();
     }
 
-    public void addThreadToShoppingList(UUID threadId) {
-        if (!mShoppingThreadsList.contains(threadId)) {
-            mShoppingThreadsList.add(threadId);
-        }
-    }
 
-    public void removeThreadFromShoppingList(UUID threadId) {
-        mShoppingThreadsList.remove(threadId);
-    }
-
-    public StashThread getThread(UUID key) {
-        // given a UUID key, look up the toString() and return thread object
-        return mThreadsData.get(key);
-    }
-
-    public void setEmbellishmentData(HashMap<UUID, StashEmbellishment> embellishmentMap, ArrayList<UUID> embellishmentList, ArrayList<UUID> stashList) {
-        // set the loaded embellishmentmap from JSON
-        mEmbellishmentData = embellishmentMap;
-        mEmbellishmentList = embellishmentList;
-        mStashEmbellishmentList = stashList;
-    }
-
-    public void setEmbellishmentShoppingList(ArrayList<UUID> shoppingList) {
-        // set the embellishment shopping list (provided by the shopping list class)
-        mShoppingEmbellishmentList = shoppingList;
-    }
-
-    public HashMap<UUID, StashEmbellishment> getEmbellishmentData() {
-        // pass embellishmentmap for saving stash/building links
-        return mEmbellishmentData;
-    }
-
-    public ArrayList<UUID> getEmbellishmentList() {
-        // pass embellishmentlist for list adapters
-        return mEmbellishmentList;
-    }
-
-    public ArrayList<UUID> getEmbellishmentStashList() {
-        // pass the stash embellishmentlist for list adapters
-        return mStashEmbellishmentList;
-    }
-
-    public ArrayList<UUID> getEmbellishmentShoppingList() {
-        // pass the embellishment shopping list for list adapters
-        Collections.sort(mShoppingEmbellishmentList, new StashEmbellishmentComparator(mAppContext));
-        return mShoppingEmbellishmentList;
-    }
-
-    public void addEmbellishmentToStash(UUID embellishmentId) {
-        // needed to update the list as the user edits the quantities of embellishments owned
-        // only one entry per embellishment on the list
-        if (!mStashEmbellishmentList.contains(embellishmentId)) {
-            mStashEmbellishmentList.add(embellishmentId);
-        }
-    }
-
-    public void removeEmbellishmentFromStash(UUID embellishmentId) {
-        // needed to update the list as the user edits the quantities of embellishments owned
-        mStashEmbellishmentList.remove(embellishmentId);
-    }
-
-    public void addEmbellishmentToShoppingList(UUID embellishmentId) {
-        if (!mShoppingEmbellishmentList.contains(embellishmentId)) {
-            mShoppingEmbellishmentList.add(embellishmentId);
-        }
-    }
-
-    public void removeEmbellishmentFromShoppingList(UUID embellishmentId) {
-        mShoppingEmbellishmentList.remove(embellishmentId);
-    }
-
-    public StashEmbellishment getEmbellishment(UUID key) {
-        // given a UUID key, look up the toString() and return embellishment object
-        return mEmbellishmentData.get(key);
-    }
-
-    public void setFabricData(HashMap<UUID, StashFabric> fabricMap, ArrayList<UUID> fabricList, ArrayList<UUID> stashFabricList) {
-        // set loaded fabricMap from JSON
-        mFabricData = fabricMap;
-        mFabricList = fabricList;
-        mStashFabricList = stashFabricList;
-    }
-
-    public void setFabricForList(ArrayList<StashPattern> patternList) {
-        mFabricForList = patternList;
-    }
-
-    public HashMap<UUID, StashFabric> getFabricData() {
-        // pass fabricmap for saving stash/building links
-        return mFabricData;
-    }
-
-    public ArrayList<UUID> getStashFabricList() {
-        return mStashFabricList;
-    }
-
-    public ArrayList<UUID> getFabricList() {
-        // pass fabriclist for list adapters
-        return mFabricList;
-    }
-
-    public ArrayList<StashPattern> getFabricForList() {
-        // return list of patterns marked as kitted and needing fabric for the shopping list
-        return mFabricForList;
-    }
-
-    public StashFabric getFabric(UUID key) {
-        // given a UUID key, look up the toString() and return fabric object
-        return mFabricData.get(key);
-    }
-
-    public void removeFabricFromStash(UUID key) {
-        mStashFabricList.remove(key);
-    }
-
+    // pattern-specific stash functions
     public void setPatternData(ArrayList<StashPattern> patternList) {
         // set the loaded patternlist from JSON
         mPatternsData = patternList;
@@ -302,6 +178,120 @@ public class StashData {
         mFabricForList.remove(pattern);
     }
 
+
+    // fabric-specific stash functions
+    public void setFabricData(HashMap<UUID, StashFabric> fabricMap, ArrayList<UUID> fabricList, ArrayList<UUID> stashFabricList) {
+        // set loaded fabricMap from JSON
+        mFabricData = fabricMap;
+        mFabricList = fabricList;
+        mStashFabricList = stashFabricList;
+    }
+
+    public void setFabricForList(ArrayList<StashPattern> patternList) {
+        mFabricForList = patternList;
+    }
+
+    public ArrayList<UUID> getStashFabricList() {
+        return mStashFabricList;
+    }
+
+    public ArrayList<UUID> getFabricList() {
+        // pass fabriclist for list adapters
+        return mFabricList;
+    }
+
+    public ArrayList<StashPattern> getFabricForList() {
+        // return list of patterns marked as kitted and needing fabric for the shopping list
+        return mFabricForList;
+    }
+
+    public StashFabric getFabric(UUID key) {
+        // given a UUID key, look up the toString() and return fabric object
+        return mFabricData.get(key);
+    }
+
+    public void removeFabricFromStash(UUID key) {
+        mStashFabricList.remove(key);
+    }
+
+    public void addFabric(StashFabric fabric) {
+        // adds fabric to the hashmap and to the list of IDs powering the adapter
+        mFabricData.put(fabric.getId(), fabric);
+        mFabricList.add(fabric.getId());
+        mStashFabricList.add(fabric.getId());
+    }
+
+    public void deleteFabric(StashFabric fabric) {
+        // clean up associations with this fabric
+        if (fabric.isAssigned()) {
+            StashPattern pattern = fabric.usedFor();
+            pattern.setFabric(null);
+        }
+
+        // removes fabric from the hashmap and the list powering the adapter
+        mFabricData.remove(fabric.getId());
+        mFabricList.remove(fabric.getId());
+        mStashFabricList.remove(fabric.getId());
+    }
+
+
+    //thread specific stash functions
+    public void setThreadData(HashMap<UUID, StashThread> threadMap, ArrayList<UUID> threadList, ArrayList<UUID> stashList) {
+        // set the loaded threadmap from JSON
+        mThreadsData = threadMap;
+        mThreadsList = threadList;
+        mStashThreadsList = stashList;
+    }
+
+    public void setThreadShoppingList(ArrayList<UUID> shoppingList) {
+        // store the shopping list provided by the shoppinglist creator
+        mShoppingThreadsList = shoppingList;
+    }
+
+    public ArrayList<UUID> getThreadList() {
+        // pass master threadlist for list adapters
+        return mThreadsList;
+    }
+
+    public ArrayList<UUID> getThreadStashList() {
+        // pass the stash-only threadlist for list adapters
+        return mStashThreadsList;
+    }
+
+    public ArrayList<UUID> getThreadShoppingList() {
+        // pass the shopping threadlist for list adapters
+        Collections.sort(mShoppingThreadsList, new StashThreadComparator(mAppContext));
+        return mShoppingThreadsList;
+    }
+
+    public void addThreadToStash(UUID threadId) {
+        // needed to update the list as the user edits the quantities of thread owned
+        // only one entry per thread on the list
+        if (!mStashThreadsList.contains(threadId)) {
+            mStashThreadsList.add(threadId);
+        }
+    }
+
+    public void removeThreadFromStash(UUID threadId) {
+        // needed to update the list as the user edits the quantities of thread owned
+        mStashThreadsList.remove(threadId);
+    }
+
+    public void addThreadToShoppingList(UUID threadId) {
+        if (!mShoppingThreadsList.contains(threadId)) {
+            mShoppingThreadsList.add(threadId);
+        }
+    }
+
+    public void removeThreadFromShoppingList(UUID threadId) {
+        mShoppingThreadsList.remove(threadId);
+    }
+
+    public StashThread getThread(UUID key) {
+        // given a UUID key, look up the toString() and return thread object
+        return mThreadsData.get(key);
+    }
+
     public void addThread(StashThread thread) {
         // adds a thread to the hashmap and to the list of IDs powering the adapter
         mThreadsData.put(thread.getId(), thread);
@@ -325,24 +315,62 @@ public class StashData {
         mShoppingThreadsList.remove(thread.getId());
     }
 
-    public void addFabric(StashFabric fabric) {
-        // adds fabric to the hashmap and to the list of IDs powering the adapter
-        mFabricData.put(fabric.getId(), fabric);
-        mFabricList.add(fabric.getId());
-        mStashFabricList.add(fabric.getId());
+
+    // embellishment specific stash functions
+    public void setEmbellishmentData(HashMap<UUID, StashEmbellishment> embellishmentMap, ArrayList<UUID> embellishmentList, ArrayList<UUID> stashList) {
+        // set the loaded embellishmentmap from JSON
+        mEmbellishmentData = embellishmentMap;
+        mEmbellishmentList = embellishmentList;
+        mStashEmbellishmentList = stashList;
     }
 
-    public void deleteFabric(StashFabric fabric) {
-        // clean up associations with this fabric
-        if (fabric.isAssigned()) {
-            StashPattern pattern = fabric.usedFor();
-            pattern.setFabric(null);
-        }
+    public void setEmbellishmentShoppingList(ArrayList<UUID> shoppingList) {
+        // set the embellishment shopping list (provided by the shopping list class)
+        mShoppingEmbellishmentList = shoppingList;
+    }
 
-        // removes fabric from the hashmap and the list powering the adapter
-        mFabricData.remove(fabric.getId());
-        mFabricList.remove(fabric.getId());
-        mStashFabricList.remove(fabric.getId());
+    public ArrayList<UUID> getEmbellishmentList() {
+        // pass embellishmentlist for list adapters
+        return mEmbellishmentList;
+    }
+
+    public ArrayList<UUID> getEmbellishmentStashList() {
+        // pass the stash embellishmentlist for list adapters
+        return mStashEmbellishmentList;
+    }
+
+    public ArrayList<UUID> getEmbellishmentShoppingList() {
+        // pass the embellishment shopping list for list adapters
+        Collections.sort(mShoppingEmbellishmentList, new StashEmbellishmentComparator(mAppContext));
+        return mShoppingEmbellishmentList;
+    }
+
+    public void addEmbellishmentToStash(UUID embellishmentId) {
+        // needed to update the list as the user edits the quantities of embellishments owned
+        // only one entry per embellishment on the list
+        if (!mStashEmbellishmentList.contains(embellishmentId)) {
+            mStashEmbellishmentList.add(embellishmentId);
+        }
+    }
+
+    public void removeEmbellishmentFromStash(UUID embellishmentId) {
+        // needed to update the list as the user edits the quantities of embellishments owned
+        mStashEmbellishmentList.remove(embellishmentId);
+    }
+
+    public void addEmbellishmentToShoppingList(UUID embellishmentId) {
+        if (!mShoppingEmbellishmentList.contains(embellishmentId)) {
+            mShoppingEmbellishmentList.add(embellishmentId);
+        }
+    }
+
+    public void removeEmbellishmentFromShoppingList(UUID embellishmentId) {
+        mShoppingEmbellishmentList.remove(embellishmentId);
+    }
+
+    public StashEmbellishment getEmbellishment(UUID key) {
+        // given a UUID key, look up the toString() and return embellishment object
+        return mEmbellishmentData.get(key);
     }
 
     public void addEmbellishment(StashEmbellishment embellishment) {
@@ -366,40 +394,6 @@ public class StashData {
         mEmbellishmentList.remove(embellishment.getId());
         mStashEmbellishmentList.remove(embellishment.getId());
         mShoppingEmbellishmentList.remove(embellishment.getId());
-    }
-
-    public boolean saveStash() {
-        // called at appropriate lifecycle events to save the stash to file
-        try {
-            mSerializer.saveStash(sStash);
-            Log.d(TAG, "stash saved to file");
-            return true;
-        } catch (Exception e) {
-            Log.e(TAG, "Error saving stash: ", e);
-            return false;
-        }
-    }
-
-    public void deleteStash() {
-        // clears ALL stash data and then saves it
-        mEmbellishmentData.clear();
-        mEmbellishmentList.clear();
-        mStashEmbellishmentList.clear();
-        mShoppingEmbellishmentList.clear();
-
-        mFabricData.clear();
-        mFabricList.clear();
-        mStashFabricList.clear();
-
-        mThreadsData.clear();
-        mThreadsList.clear();
-        mStashThreadsList.clear();
-        mShoppingThreadsList.clear();
-
-        mPatternsData.clear();
-        mFabricForList.clear();
-
-        saveStash();
     }
 
 }
